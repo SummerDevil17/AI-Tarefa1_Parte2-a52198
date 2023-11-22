a7deacd59f35b2a3d05340ac670ec879b14e1960
Carlos Guedes <carlosguedesss17@gmail.com>
2023-11-22 09:42:01 +0000

    a5198_Tree Printing

--- Assets/3 - Behaviour Trees/Scripts/BehaviourTree.cs
+++ Assets/3 - Behaviour Trees/Scripts/BehaviourTree.cs
@@ -14,9 +14,33 @@ public class BehaviourTree : Node
         name = behaviourName;
     }
 
+    struct NodeLevel
+    {
+        public int level;
+        public Node node;
+    }
+
     public void PrintTree()
     {
-        Debug.Log("Root Node: " + this.name);
+        string treeDebugLog = "";
+        Stack<NodeLevel> nodeStack = new Stack<NodeLevel>();
+        Node currentNode = this;
+
+        nodeStack.Push(new NodeLevel { level = 0, node = currentNode });
+
+        while (nodeStack.Count != 0)
+        {
+            NodeLevel nextNode = nodeStack.Pop();
+            treeDebugLog += ">" + new string('-', nextNode.level) + nextNode.node.name + "\n";
+
+            for (int i = nextNode.node.children.Count - 1; i >= 0; i--)
+            {
+                nodeStack.Push(new NodeLevel { level = nextNode.level + 1, node = nextNode.node.children[i] });
+            }
+        }
+        Debug.Log(treeDebugLog);
+
+        /*Debug.Log("Root Node: " + this.name);
         foreach (Node child in this.children)
         {
             Debug.Log("Child Node: " + child.name);
@@ -24,6 +48,6 @@ public class BehaviourTree : Node
             {
                 Debug.Log("Grandchild Node: " + grandchild.name);
             }
-        }
+        }*/
     }
 }
